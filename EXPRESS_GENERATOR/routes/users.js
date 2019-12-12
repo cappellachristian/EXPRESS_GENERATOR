e51624daab1676466148997db8dbe56362b25820
cappellachristian <christiancappella01@gmail.com>
2019-12-12 12:27:16 +0000

    tata è uno scorpione

--- EXPRESS_GENERATOR/routes/users.js
+++ EXPRESS_GENERATOR/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var gesuRouter = require('./routers/users/gesu');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+router.get('/GESU', function(req, res, next) {
+  res.send('PRONTONE!!!!!');
 });
 
 module.exports = router;
